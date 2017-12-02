667293485c83e20cb674f67fac83a8578b839cb5
David <davidaase@hotmail.com>
2017-12-02 12:04:07 +0100

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,66 @@
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import io.javalin.Javalin;
+import io.javalin.embeddedserver.jetty.EmbeddedJettyFactory;
+import io.prometheus.client.exporter.HTTPServer;
+import java.io.IOException;
+import java.util.Random;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.StatisticsHandler;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import static io.javalin.ApiBuilder.get;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        StatisticsHandler statisticsHandler = new StatisticsHandler();
+        QueuedThreadPool queuedThreadPool = new QueuedThreadPool(200, 8, 60_000);
+        initializePrometheus(statisticsHandler, queuedThreadPool);
+
+        Javalin app = Javalin.create()
+            .port(7070)
+            .embeddedServer(new EmbeddedJettyFactory(() -> {
+                Server server = new Server(queuedThreadPool);
+                server.setHandler(statisticsHandler);
+                return server;
+            }))
+            .start();
+
+        app.routes(() -> {
+            get("/1", ctx -> ctx.result("Hello World"));
+            get("/2", ctx -> {
+                Thread.sleep((long) (Math.random() * 2000));
+                ctx.result("Slow Hello World");
+            });
+            get("/3", ctx -> ctx.redirect("/2"));
+            get("/4", ctx -> ctx.status(400));
+            get("/5", ctx -> ctx.status(500));
+        });
+
+        while (true) {
+            spawnRandomRequests();
+        }
+
+    }
+
+    private static void initializePrometheus(StatisticsHandler statisticsHandler, QueuedThreadPool queuedThreadPool) throws IOException {
+        StatisticsHandlerCollector.initialize(statisticsHandler);
+        QueuedThreadPoolCollector.initialize(queuedThreadPool);
+        HTTPServer prometheusServer = new HTTPServer(7080);
+    }
+
+    private static void spawnRandomRequests() throws InterruptedException {
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < new Random().nextInt(50); i++) {
+                    Unirest.get("http://localhost:7070/1").asString(); // we want a lot more "200 - OK" traffic
+                    Unirest.get("http://localhost:7070/" + (1 + new Random().nextInt(5))).asString(); // hit a random (1-5) endpoint
+                }
+            } catch (UnirestException ignored) {
+            }
+        }).start();
+        Thread.sleep((int) (Math.random() * 250));
+    }
+
+}
